b9e6f1fdf0257d67db6b804a5a46bc878b58f512
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-06-03 11:47:06 +0400

    Deconstruct

--- Net 06. Deconstructing, Null Indexers, operator overloading/Program.cs
+++ Net 06. Deconstructing, Null Indexers, operator overloading/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿// Deconstructing, tuple
+Car car = new Car(Model: "Ferrari", Year: 2024);
+//int year = car.Year;
+//string model = car.Model;
+
+var (mod, yr) = car;
+Console.WriteLine($"model: {mod} - year: {yr}");
+
+//int numb = default;
+//bool check = default;
+//string name = default;
+
+//(numb, check, name) = Foo();
+
+//Console.WriteLine($"numb = {numb}; check = {check}; name = {name};");
+
+//(int number, _, string some) = Foo();
+//Console.WriteLine(some);
+var returnValues = Foo();
+Console.WriteLine(returnValues.Item1); // 697
+Console.WriteLine(returnValues.Item2); // true
+Console.WriteLine(returnValues.Item3); // Salam
+
+
+
+(int, bool, string) Foo()
+{
+    return (697, true, "Salam");
+}
+
+class Car
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public Car(string Model, int Year)
+    {
+        this.Model = Model;
+        this.Year = Year;
+    }
+
+    public void Deconstruct(out string model, out int year)
+    {
+        model = Model;
+        year = Year;
+    }
+}
