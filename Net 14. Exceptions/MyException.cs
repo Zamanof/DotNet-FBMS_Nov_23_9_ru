04c95aec8dcc2608f3156832557bbe657c101f52
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-06-28 11:19:23 +0400

    Exceptions

---
+++ Net 14. Exceptions/MyException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Net_14._Exceptions;
+/*
+ Pravila sozdaniya svoix isklyucheniy:
+    - isklyucheniye doljno nasledovatsya ot klassa ApplicationException
+    - u isklyucheniya doljen bit ukazan atribut [Serializable]
+    - u isklyucheniya doljen bit konstruktor po umolchaniyu
+    - u isklyucheniya doljen bit perqrujenniy konstruktor dlya ustonovki svoystva Message
+    - u isklyucheniya doljen bit perqrujenniy konstruktor dlya obrobotki vnutrennix isklyucheniy
+    - doljen bit konstruktor vipolnyayushiy serializasiyu tipa
+ 
+ */
+[Serializable]
+public class MyException: ApplicationException
+{
+    public MyException(){}
+    public MyException(string message): base(message) { }
+    public MyException(string message, Exception inner): base(message, inner) { }
+
+    protected MyException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+}
---
+++ Net 14. Exceptions/Program.cs
@@ -0,0 +1,58 @@
+﻿// Exceptions
+using Net_14._Exceptions;
+
+int numb1 = 3;
+int numb2 = 0;
+int result = default;
+// Vse isklyucheniya nasleduyutsya ot bazovoq klassa Exception
+// Ot Exception nasleduyutsya 2 klassa : SystemException, ApplicationException
+
+try
+{
+    //result = numb1 / numb2;
+    Foo(3, 0);
+
+}
+catch (DivideByZeroException ex)
+{
+    //Console.WriteLine(ex.Message);
+    //Console.WriteLine(ex.StackTrace);
+    Console.WriteLine("Deleniya na nol eto ujasno! fooo!");
+}
+catch(MyException ex)
+{
+    Console.WriteLine("Own exception handled");
+}
+
+// exception filters
+catch(Exception ex) when(ex.InnerException != null)
+{
+
+}
+
+finally
+{
+    Console.WriteLine("Block Finally");
+}
+
+Console.WriteLine("End of code");
+
+int Foo(int a, int b)
+{
+    int result = default;
+    int[] arr = new int[3] { 25, 78, 9};
+
+    try
+	{
+		result = a / b;
+        //Console.WriteLine(arr[5]);
+    }
+	catch (DivideByZeroException ex)
+	{
+        Console.WriteLine("Catch in method Foo");
+        //throw new Exception("Hakuna matata");
+        throw new MyException("This is Spartaaaaaaaaaaaaaaa!");
+
+	}
+    return result;
+}
