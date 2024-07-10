40185b7bf511d8a70661b6af8ead35f060ef69b8
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-07-10 09:33:14 +0400

    Singleton

--- Net 17. Singleton pattern/Program.cs
+++ Net 17. Singleton pattern/Program.cs
@@ -1,11 +1,14 @@
 ﻿President president;
 president = President.getInstance("Fransua", "Macrone", 45);
-Console.WriteLine(president);
-//president = null;
-President president2 = null;
-president2 = President.getInstance("Vladimir", "Putin", 75);
+//Console.WriteLine(president);
+President president1 = President.getInstance("Fransua", "Macrone", 45);
+//Console.WriteLine(president1);
+//Console.WriteLine(president1 == president);
+President president2 = President.getInstance("Fransua", "Macrone", 45);
+Console.WriteLine(president1 == president2);
 Console.WriteLine(president == president2);
-Console.WriteLine(president2);
+
+
 class President
 {
     public string Name { get; set; }
@@ -14,14 +17,14 @@ class President
 
     private static President _instance = null;
 
-    public  President(string name, string surname, int age)
+    public President(string name, string surname, int age)
     {
         Name = name;
         Surname = surname;
         Age = age;
     }
 
-    public static President getInstance(string name, string surname, int age)
+   static public  President getInstance(string name, string surname, int age)
     {
         if (_instance is null)
         {
